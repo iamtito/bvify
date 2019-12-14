aee82558c2649ba8b07f1cdd7d7e2672072f362c
kabir Bolatito <kabirbolatito@Talents-MBP-6.home>
2019-12-14 13:15:34 -0500

    first migration

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Create models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    # Delete a post once the author gets deleted
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
