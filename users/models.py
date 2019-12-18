96cbf1ecd4327f2e9373ee752abcca44dba95553
kabir Bolatito <kabirbolatito@Talents-MBP-6.home>
2019-12-18 00:05:26 -0500

    Pagination done, isolated user page, done.

--- users/models.py
+++ users/models.py
@@ -1,10 +1,24 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    job = models.CharField(default='',max_length=100)
+    description = models.TextField(default='Doing something awesome one step at a time',max_length=200)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        img = Image.open(self.image.path, mode='r')
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300,300)
+            img.thumbnail(output_size)
+            img = img.convert('RGB')
+            img.save(self.image.path)
