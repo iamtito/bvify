78b479b9ac7d119cb01c06038a63c478c3f65bf1
kabir Bolatito <kabirbolatito@Talents-MBP-6.home>
2019-12-18 00:05:26 -0500

    Pagination done, isolated user page, done.

--- django_blog/urls.py
+++ django_blog/urls.py
@@ -26,9 +26,10 @@ urlpatterns = [
     path('profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    path('blog/', include('blog.urls')),
+    path('', include('blog.urls')),
+    # path('blog/', include('blog.urls')), #<-- uncomment to specify it start from ...com/blog
 ]
 
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 class UserRegistrationForm(UserCreationForm):
     email = forms.EmailField()
@@ -8,3 +9,18 @@ class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1','password2']
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+        
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    job = forms.CharField(max_length=100, required=False, label='Profession')
+    description = forms.CharField(widget=forms.Textarea,max_length=200, required=False, label='Describe yourself')
+
+    class Meta:
+        model = Profile
+        fields = ['job','description','image']
